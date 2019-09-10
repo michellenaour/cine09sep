045bd9927a193383a74077914fa76fca1e7a0c6a
michellenaour <m.naour01@ufromail.com>
2019-09-09 23:11:48 -0300

    primercommit

---
+++ src/main/java/ventaentradascine.java
@@ -0,0 +1,174 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ventaentradascine {
+    static Random azar= new Random();
+    static Scanner teclado= new Scanner(System.in);
+    static ArrayList<int[][]> salas = new ArrayList<int[][]>();
+    static int numeroDeSalas;
+    static String [] alfabeto = new String [] {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+
+
+    public static void main (String[]args){
+        numeroDeSalas=leer_Numero_de_salas();
+        hacerCine();
+        boolean salir=false;
+        venta();
+        venta();
+    }
+
+    public static int leer_Numero_de_salas(){
+        boolean estado= false;
+        int numero=-1;
+        while (estado==false){
+            numero=leerNumero(" el número de salas del cine");
+            estado=validarNroSalas(numero);
+            if(estado==false){ System.out.println("El número de salas debe ser mayor a 0.");}
+        }
+        return numero;
+    }
+
+    public static boolean validarNroSalas(int n){
+        boolean e = false;
+        if(n>0){e=true;}
+        return e;
+    }
+
+    public static void hacerCine(){
+        for(int i=0;i<numeroDeSalas;i++){
+            int c= nroAlAzar(26);
+            int f = nroAlAzar(100);
+            int [][] sala = new int[f][c];
+            salas.add(sala);
+        }
+    }
+
+    public static void venta(){
+        int nrodeSala = elegirpelicula();
+        int [][] salaSeleccionada = salas.get(nrodeSala);
+        mostarSala(salaSeleccionada);
+        boolean disponible = false;
+        int filas=-1, columnas=-1;
+
+        while(disponible==false){
+            filas= ingresarFila(salaSeleccionada)-1;
+            columnas= ingresarColumnas(salaSeleccionada)-1;
+            disponible=disponibilidadButaca(salaSeleccionada, filas, columnas);
+            if (disponible==false){
+                System.out.print("la butaca está ocupada");
+            }
+        }
+
+        ocuparbutaca(salaSeleccionada,filas,columnas);
+        mostarSala(salaSeleccionada);
+    }
+
+    public static int ingresarColumnas(int [][] sala){
+        boolean existe=false;
+        String letra= "";
+        int index = -1;
+        while (existe==false){
+            letra=leerletra();
+            letra=letra.toLowerCase();
+            String alphabet = "abcdefghijklmnopqrstuvwxyz";
+            index=1+alphabet.indexOf(letra.charAt(0));
+            if (index<=sala[1].length && index>=1){
+                existe=true;
+            }
+
+        }
+
+        return index;
+    }
+
+    public static String leerletra (){
+        String letra="";
+        while(letra.length()!=1){
+            System.out.println("ingrese letra");
+            letra="";
+            letra=teclado.nextLine();
+            if(letra.length()!=0){System.out.println("error");}
+        }
+      return letra;
+    }
+
+    public static boolean disponibilidadButaca(int[][] sala, int f, int c){
+        boolean disponible = false;
+        if(sala[f][c]==0){ disponible=true;}
+        return disponible;
+    }
+
+    public static int ingresarFila(int[][] sala){
+        boolean est=false;
+        int max= sala.length;
+        int f=0;
+        while (est==false){
+            f=leerNumero("ingrese la Fila de la butaca: 1-"+ max);
+            est=validarFila(f,max);
+            if (est==false){System.out.print("Ingrese un valor válido ");}
+        }
+        return f;
+    }
+
+    public static boolean validarFila(int f,int max){
+        boolean e= false;
+        if(f>=0 && f<max){e=true;}
+        return e;
+    }
+
+    public static void mostarSala(int [][] sala){
+        for(int a=0; a< sala[1].length; a++) {
+            System.out.print(" ");
+            System.out.print("  " + alfabeto[a] + "   ");
+        }
+        System.out.print("\n");
+        for (int x=0; x < sala.length; x++) {
+            for (int y = 0; y < sala[x].length; y++){ System.out.print(" | " + sala[x][y] + " | ");}
+            System.out.println(x+1);
+        }
+
+    }
+
+    public static int elegirpelicula(){
+        boolean estado= false;
+        int pelicula=-1;
+        while(estado==false){
+            pelicula=leerNumero("ingrese el numero de la pelicula que desee ver");
+            estado=validarPelicula(pelicula);
+            if (estado==false){System.out.println("Error! Ingresó un número inválido, vuelva a intentar");}
+        }
+        return pelicula-1;
+    }
+
+    public static boolean validarPelicula(int nro){
+        boolean e= false;
+        if(nro>=0 && nro<=numeroDeSalas){ e=true; }
+        return e;
+    }
+
+    public static int leerNumero(String txt){
+        System.out.println("ingrese"+txt);
+        while (!teclado.hasNextInt()) {
+            System.out.println("Error! debe ingresar un número");
+            teclado.nextLine();
+        }
+        int number = teclado.nextInt();
+        return number;
+    }
+
+    public static void ocuparbutaca(int [][] sala, int f, int c){
+        sala[f][c]=1;
+    }
+
+    public static int nroAlAzar(int max){
+        return azar.nextInt(max);
+    }
+
+
+
+
+
+}
+
+
